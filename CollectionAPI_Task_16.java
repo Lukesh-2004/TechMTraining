720f051621d9fb3881c75642a805433a31cc68e5
D Lukesh Rajaraman <lukesh.reach@gmail.com>
2025-02-16 19:53:22 +0530

    Update CollectionAPI_Task_16.java

--- CollectionAPI_Task_16.java
+++ CollectionAPI_Task_16.java
@@ -1,1 +1,14 @@
+package lukesh;
 
+
+import java.util.LinkedList;
+
+public class InsertFirstLast {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.addFirst(10);
+        list.addLast(50);
+
+        System.out.println("Updated LinkedList: " + list);
+    }
+}
