08bfcb6bb0b679861ef7598c2a12fbc144293d4c
D Lukesh Rajaraman <lukesh.reach@gmail.com>
2025-02-16 20:00:23 +0530

    Update CollectionAPI_Task_5.java

--- CollectionAPI_Task_5.java
+++ CollectionAPI_Task_5.java
@@ -1,1 +1,12 @@
+package lukesh;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class UpdateElement {
+	public static void main(String[] args) {
+		List<String> cities = new ArrayList<>(List.of("New York", "Paris", "Tokyo"));
+		cities.set(1, "London"); // Update Paris to London
+		System.out.println("Updated List: " + cities);
+	}
+}
