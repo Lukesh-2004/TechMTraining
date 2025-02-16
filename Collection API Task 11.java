cb32ffe3b58391226e7f1ddf59216f05e1be9b9b
D Lukesh Rajaraman <lukesh.reach@gmail.com>
2025-02-16 19:45:10 +0530

    Update Collection API Task 11.java

--- Collection API Task 11.java
+++ Collection API Task 11.java
@@ -1,1 +1,15 @@
+package lukesh;
 
+import java.util.LinkedList;
+
+public class AppendToLinkedList {
+	public static void main(String[] args) {
+		LinkedList<String> list = new LinkedList<>();
+		list.add("Red");
+		list.add("Blue");
+		list.add("Green");
+
+		list.addLast("Yellow"); // Append to the end
+		System.out.println("Updated LinkedList: " + list);
+	}
+}
