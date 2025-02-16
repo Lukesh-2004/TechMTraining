63b88af93295e9283240b82f7405c6c605444fe8
D Lukesh Rajaraman <lukesh.reach@gmail.com>
2025-02-16 19:50:27 +0530

    Update Collection API Task 14.java

--- Collection API Task 14.java
+++ Collection API Task 14.java
@@ -1,1 +1,15 @@
+package lukesh;
 
+import java.util.LinkedList;
+import java.util.Iterator;
+
+public class ReverseIterateLinkedList {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>(java.util.List.of("One", "Two", "Three"));
+
+        Iterator<String> iterator = list.descendingIterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
