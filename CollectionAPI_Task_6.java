894c5e618165bdb132ab1511c2cd9cac9ea8bae1
D Lukesh Rajaraman <lukesh.reach@gmail.com>
2025-02-16 20:01:15 +0530

    Update CollectionAPI_Task_6.java

--- CollectionAPI_Task_6.java
+++ CollectionAPI_Task_6.java
@@ -1,1 +1,12 @@
+package lukesh;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemoveThirdElement {
+	public static void main(String[] args) {
+		List<String> languages = new ArrayList<>(List.of("Java", "Python", "C++", "JavaScript"));
+		languages.remove(2); // Remove C++
+		System.out.println("Updated List: " + languages);
+	}
+}
