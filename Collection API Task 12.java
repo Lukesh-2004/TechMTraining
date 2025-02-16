67387285e4a646ed3ae6357cee85d18dcf9fce39
D Lukesh Rajaraman <lukesh.reach@gmail.com>
2025-02-16 19:46:07 +0530

    Update Collection API Task 12.java

--- Collection API Task 12.java
+++ Collection API Task 12.java
@@ -1,1 +1,14 @@
+import java.util.LinkedList;
 
+public class IterateLinkedList {
+	public static void main(String[] args) {
+		LinkedList<Integer> list = new LinkedList<>();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+
+		for (Integer num : list) {
+			System.out.println(num);
+		}
+	}
+}
